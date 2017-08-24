f0bc9cee85b52757139104928157e5308be4dc32
MartkCz <antik494@gmail.com>
2017-08-24 09:04:34 +0200

    added mailer, baseDir

--- src/BaseEmail.php
+++ src/BaseEmail.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace ProLib\Emails;
 
 use Nette\Application\UI\ITemplateFactory;
+use Nette\Mail\IMailer;
 
 abstract class BaseEmail {
 
@@ -14,16 +15,26 @@ abstract class BaseEmail {
 	/** @var array */
 	private $from = [null, null];
 
-	public function __construct(ITemplateFactory $templateFactory) {
+	/** @var string */
+	protected $baseDir;
+
+	/** @var IMailer */
+	private $mailer;
+
+	public function injectComponents(string $baseDir, ITemplateFactory $templateFactory, IMailer $mailer) {
 		$this->templateFactory = $templateFactory;
+		$this->mailer = $mailer;
+		$this->baseDir = $baseDir;
 	}
+
+	abstract protected function getTemplateFile(): string;
 
 	public function setDefaultFrom(string $from, $name): void {
 		$this->from = [$from, $name];
 	}
 
 	public function create(): Email {
-		$email = new Email($this->templateFactory, ...$this->from);
+		$email = new Email($this->templateFactory, $this->mailer, $this->getTemplateFile(), ...$this->from);
 
 		return $email;
 	}
